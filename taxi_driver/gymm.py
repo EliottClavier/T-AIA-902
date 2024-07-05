1a2d41309e9929ae1f9133fc1d8bbae26c912b08
Eliott Clavier <clavier.eliott.el@gmail.com>
2024-07-05 16:35:43 +0200

    [HOTFIX] Fixed gym env registring which can set max_n_steps to infinite

--- taxi_driver/gymm.py
+++ taxi_driver/gymm.py
@@ -5,16 +5,16 @@ from pathlib import Path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
 from common.params import TaxiDriverParams
-from common.algorithms import QLearning, SARSA, BruteForce
-from common.policies import DecayedEpsilonGreedy, Random
+from common.algorithms import QLearning
+from common.policies import DecayedEpsilonGreedy
 from common.environments import TaxiDriver
 from common.plots import TaxiDriverPlots
 
 
 def main():
     params = TaxiDriverParams(
-        n_episodes=250,
-        n_runs=1,  # attention sur le random laissez a 1
+        n_episodes=10000,
+        n_runs=10,
         learning_rate=0.85,
         gamma=0.99,
         epsilon=1.0,
@@ -22,7 +22,7 @@ def main():
         manual_decay_rate=0.001,
         random_seed=True,
         seed=123,
-        max_n_steps=25000,
+        max_n_steps=100,
         savefig_folder=Path("./static/img/taxi_driver/"),
         savemodel_folder=Path("./static/models/taxi_driver/"),
     )
